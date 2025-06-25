880d91921c2626bfc5358f72350aee0d866c80a3
havocr <jawvvt@gmail.com>
2025-06-25 11:46:38 +0300

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#include <conio.h> 
+
+int main() {
+    int myage;
+    int votingage =18;
+    cout << "enter your age: ";
+    cin >> myage;
+    if(myage>= votingage)
+    {
+        cout << "unc";
+    getch();
+        
+     
+    } else {
+        cout << " lil bro";
+    
+        getch();
+
+    }
+
+}
+
+
+
+  
+
+  
